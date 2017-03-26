099b0dc22c88c517f7847efac11b49468ce0c5ec
Dmitriy Tokarev <freemiumhead@gmail.com>
2017-03-26 15:15:46 +0300

    add little description

--- src/components/Test1/Test1.js
+++ src/components/Test1/Test1.js
@@ -8,6 +8,7 @@ const Test1 = (props) => {
 
   return (
     <div className={testClass}>
+      <p>Здесь я всю голову сломал как мне отодвинуть цифру от круга, как это сделать без дополнительных элементов не придумал.</p>
       <div className="block1">
         <div className="block1__child">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium dicta dolor dolorum in laboriosam mollitia?</div>
         <div className="block1__child">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium dicta dolor dolorum in laboriosam mollitia?</div>
--- src/components/Test5/Test5.js
+++ src/components/Test5/Test5.js
@@ -31,7 +31,32 @@ const Test5 = (props) => {
 
   return (
     <div className={testClass}>
+      <pre>
+        <code>
+          {`// нагуглил
+          const Singleton = (function () {
+            let instance
 
+            return function Construct_singletone () {
+              if (instance) {
+                return instance;
+              }
+              if (this && this.constructor === Construct_singletone) {
+                instance = this
+              } else {
+                return new Construct_singletone()
+              }
+            }
+          }());
+
+          var a = new Singleton();
+          var b = new Singleton();
+          var c = Singleton();
+          console.log(a === b, a === c, b === c); // true
+          console.log(a instanceof Singleton);    // true
+          console.log(b instanceof Singleton);    // true`}
+        </code>
+      </pre>
     </div>
   )
 }

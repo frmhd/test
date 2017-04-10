e15a4a0c7374b74d98068e5aac8a0d93b3f92649
Dmitriy Tokarev <freemiumhead@gmail.com>
2017-04-10 19:42:19 +0300

    add anygrid

--- src/components/Test2/Test2.js
+++ src/components/Test2/Test2.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import './anygrid.css'
 import './Test2.css'
 
 const Test2 = (props) => {
@@ -8,6 +9,7 @@ const Test2 = (props) => {
 
   return (
     <div className={testClass}>
+      <h3>На флексах (пофикшено)</h3>
       <div className="block2">
         <div className="block2__child">1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
         <div className="block2__child">2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
@@ -15,6 +17,19 @@ const Test2 = (props) => {
         <div className="block2__child">4 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
         <div className="block2__child">5 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
         <div className="block2__child">6 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
+      </div>
+      <h3>На флоатах</h3>
+      <div className="block2-float g-12">
+        <div className="g-row">
+          <div className="block2-float__child g-col-1 g-span-4">1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
+          <div className="block2-float__child g-col-5 g-span-4">2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
+          <div className="block2-float__child g-col-9 g-span-4">3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
+        </div>
+        <div className="g-row">
+          <div className="block2-float__child g-col-1 g-span-4">4 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
+          <div className="block2-float__child g-col-5 g-span-4">5 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
+          <div className="block2-float__child g-col-9 g-span-4">6 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
+        </div>
       </div>
     </div>
   )

165db1ce012aa1c6726815e885b5ea361fd7ab85
Dmitriy Tokarev <freemiumhead@gmail.com>
2017-04-11 12:11:34 +0300

    add floats in test 2

--- src/components/Test2/Test2.js
+++ src/components/Test2/Test2.js
@@ -19,17 +19,13 @@ const Test2 = (props) => {
         <div className="block2__child">6 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
       </div>
       <h3>На флоатах</h3>
-      <div className="block2-float g-12">
-        <div className="g-row">
-          <div className="block2-float__child g-col-1 g-span-4">1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
-          <div className="block2-float__child g-col-5 g-span-4">2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
-          <div className="block2-float__child g-col-9 g-span-4">3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
-        </div>
-        <div className="g-row">
-          <div className="block2-float__child g-col-1 g-span-4">4 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
-          <div className="block2-float__child g-col-5 g-span-4">5 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
-          <div className="block2-float__child g-col-9 g-span-4">6 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
-        </div>
+      <div className="block2-float">
+        <div className="block2-float__child">1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
+        <div className="block2-float__child">2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
+        <div className="block2-float__child">3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
+        <div className="block2-float__child">4 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
+        <div className="block2-float__child">5 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
+        <div className="block2-float__child">6 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, architecto aspernatur aut cumque dolorum est, et excepturi facilis, ipsam itaque natus nihil odio rem sed ut vel veritatis voluptatum?</div>
       </div>
     </div>
   )

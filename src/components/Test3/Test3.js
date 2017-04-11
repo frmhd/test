8fa8784f60ca0c137f0067ff6d724fa5312e30a9
Dmitriy Tokarev <freemiumhead@gmail.com>
2017-04-11 12:52:32 +0300

    add loops in test 4

--- src/components/Test3/Test3.js
+++ src/components/Test3/Test3.js
@@ -8,15 +8,9 @@ const Test3 = (props) => {
 
   let value
 
-  if (document.documentElement.lang === "en") {
-    value = '$ 99.99'
-  } else {
-    value = '99.99 Р'
-  }
-
   return (
     <div className={testClass}>
-      <div className="value">{value}</div>
+      <div className="value">99.99</div>
     </div>
   )
 }
